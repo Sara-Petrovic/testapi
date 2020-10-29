4f0fc931af2f151ae2b68e018f6bd5a8f92cef04
Sara Petrovic <sarapetrovicue@gmail.com>
2020-10-29 22:29:58 +0100

    dodat Database.php

---
+++ Database.php
@@ -0,0 +1,35 @@
+<?php
+
+class Database
+{
+    private $hostname = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname;
+    private $dblink;
+    private $result;
+    private $records;
+    private $affected;
+
+    function __construct($par_dbname)
+    {
+        $this->dbname = $par_dbname;
+        $this->Connect();
+    }
+    function Connect(){
+        $this->dblink = new mysqli($this->hostname,$this->username,$this->password,$this->dbname);
+        if($this->dblink->connect_error){
+            printf("Konekcija neuspesna: %a\n",$this->dblink->connect_error);
+            exit()
+        }
+        $this->dblink->set_charset("utf8");
+    }
+    function ExecuteQuery($query){
+        if($this->result = $this->dblink->query($query)){
+            if(isset($this->result->num_rows)){
+                
+            }
+        }
+    }
+}
+?>
